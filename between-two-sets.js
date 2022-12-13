2d572115ed31b4565c40be3ca52e965d1c174c6e
Lucas Duarte <l.duarte.mk@gmail.com>
2022-12-13 13:00:03 -0300

    Update between-two-sets.js

--- between-two-sets.js
+++ between-two-sets.js
@@ -1,1 +1,45 @@
+// Link para o desafio
+// https://www.hackerrank.com/challenges/between-two-sets/
 
+//Solução
+
+function getTotalX(a, b) {
+    const largestNumber = Math.max(...b)
+    
+    const multiplicationArray = []
+    
+    a.forEach(value => {
+        let counter = 0;
+        while(counter < largestNumber) {
+            counter += value
+            multiplicationArray.push(counter)
+        }
+    })
+    
+     const divionsArray = []
+     
+     b.forEach(value => {
+         for(var i = value; i > 0; i--) {
+             if(value % i === 0) {
+                 divionsArray.push(i)
+             }
+         }
+     })
+     
+     const allNumbers = [...multiplicationArray, ...divionsArray]
+     
+     const obj = {}
+     allNumbers.forEach(value => {
+         obj[value] = (obj[value] || 0) + 1
+     })
+     
+     let counter = 0;
+     Object.keys(obj).forEach(key => {
+         if(obj[key] === (a.length + b.length)) {
+             counter++
+         }
+     })
+     
+     return counter
+    
+}
